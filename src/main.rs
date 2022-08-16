ec1258088d0842b0f09b7c9d053640a1fafaf5f7
thelostword <thelostword@163.com>
2022-08-16 10:52:52 +0800

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+/*
+ * @Author: thelostword
+ * @Date: 2022-08-15 17:00:10
+ * @LastEditors: thelostword
+ * @LastEditTime: 2022-08-16 10:36:14
+ * @FilePath: \minigrep\src\main.rs
+ * Copyright (c) 2022 by 东域信息, All Rights Reserved. 
+ */
+use std::env;
+use std::process;
+use minigrep::{Config, run};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments:{}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    };
+}
+
